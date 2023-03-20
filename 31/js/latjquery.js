1fb5ed2f7b99a89d7ba1a2dbb575ac93174d2b9f
Rakhanandadrf <110873843+Rakhanandadrf@users.noreply.github.com>
2023-03-21 06:50:32 +0700

    Add files via upload

---
+++ 31/js/latjquery.js
@@ -0,0 +1,37 @@
+$( document ).ready(function() {
+    console.log( "siap" );
+})
+
+$(function (params) {
+
+    $("#isi").html("<h1>Belajar Jquery</h1>");
+ 
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                                                                                                                                                                                       
+
+
+
+
+
+
+
