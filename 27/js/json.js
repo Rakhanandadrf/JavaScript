65d1c764981afdd47811f21ce0854746bc7a9be6
Rakhanandadrf <110873843+Rakhanandadrf@users.noreply.github.com>
2023-03-21 06:46:02 +0700

    Add files via upload

---
+++ 27/js/json.js
@@ -0,0 +1,25 @@
+let tblmenu = [ 
+    {
+    
+    "idmenu" : 1,
+    "idkategori" : 1,  
+    "menu" : "apel manalagi",
+    "harga" : 3000,
+    "warna" : ["merah", "kuning", "hijau"],
+    "stok" : true,
+    "keterangan" : null
+
+},
+{
+
+    "idmenu" : 2,
+    "idkategori" : 1,  
+    "menu" : "Pisang",
+    "harga" : 2000,
+    "warna" : ["merah", "kuning", "hijau"],
+    "stok" : false,
+    "keterangan" : null
+}
+]
+
+console.log(tblmenu[0].menu);
